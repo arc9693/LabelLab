b3747b8a378e8b5948b7cb2bd839be5c5b0cdb54
arc9693 <archana.choudhary.9693@gmail.com>
2019-03-05 19:42:49 +0530

    [ADD]Home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from "./components/Home"
+import Home from "./components/Home/Home"
 
 function App() {
   return (
     <Router>
       <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-      </ul>
+      <nav class="navbar navbar-light bg-dark">
+        <Link to="/" className="navbar-brand text-light">LabelLab</Link>
+      </nav>
         <Route exact path="/" component={Home} />
       </div>
     </Router>
---
+++ front-end/src/components/ListImages/ListImages.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+export default class ListImages extends Component {
+    render() {
+      return(
+        <div>
+        <h2 className="text-center">Your images</h2>
+        <ul>
+        <li></li>
+        </ul>
+        </div>
+      )
+    }
+}
