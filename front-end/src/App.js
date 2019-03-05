4694120a883a7ff7fc861f9aa8ab596bf201cf57
arc9693 <archana.choudhary.9693@gmail.com>
2019-03-05 13:41:22 +0530

    [ADD] Router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Route exact path="/" component={Home} />
       </div>
-    );
-  }
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+    </ul>
+  );
 }
 
 export default App;
