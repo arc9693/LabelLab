2ff9b06dda1a71c548c3661471c58acc735667b1
arc9693 <archana.choudhary.9693@gmail.com>
2019-03-18 18:39:52 +0530

    [ADD] Functionality - Draw rect on click

--- front-end/src/components/LabelImage/LabelImage.js
+++ front-end/src/components/LabelImage/LabelImage.js
@@ -39,7 +39,7 @@ export default class LabelImage extends Component {
 				if(error){console.log(error);
 					this.props.history.push('*/*/*');return;}
 				const data=response.body;
-				console.log(JSON.stringify(data));
+				// console.log(JSON.stringify(data));
 				this.setState({
 					image:data[0]
 				})
@@ -64,11 +64,11 @@ export default class LabelImage extends Component {
 			<div className="container-fluid p-0">
 				<div className="row m-0">
 					<div className="col-2 details pt-5 pl-1">
-				NAME : {image.name}<br/><br/>
-				HEIGHT : {this.state.height}<br/><br/>
-				WIDTH : {this.state.width}<br/><br/>
+							NAME : {image.name}<br/><br/>
+							HEIGHT : {this.state.height}<br/><br/>
+							WIDTH : {this.state.width}<br/><br/>
 						<hr/>
-				LABEL : <input
+							LABEL : <input
 							className="Label"
 							placeholder="Label"
 							value={image.label||' '}
@@ -81,9 +81,9 @@ export default class LabelImage extends Component {
 								});
 							}}
 							autoFocus/><br/><br/>
-				COORDINATES : ({image.x},{image.y})<br/><br/>
-				LABEL HEIGHT : {image.height}<br/><br/>
-				LABEL WIDTH : {image.width}<br/><br/>
+							COORDINATES : ({image.x},{image.y})<br/><br/>
+							LABEL HEIGHT : {image.height}<br/><br/>
+							LABEL WIDTH : {image.width}<br/><br/>
 						<div className="button mt-2 p-0 m-0 row justify-content-center">
 							<button type="submit" className="btn p-1"  onClick={this.handleSubmit}>Done</button>
 						</div>
@@ -92,20 +92,34 @@ export default class LabelImage extends Component {
 						<h6 className="font-italic text-muted">Note: Drag the rectangle to mark the area</h6>
 						<div className="row justify-content-center">
 							<img ref="image" onLoad={this.onImgLoad} src={this.state.url+'uploads/'+image.name} alt={image.name} style={{position:'absolute',zIndex:'-1'}}/>
-							<Stage height={this.state.height} width={this.state.width} style={{cursor:this.state.isHovered?'crosshair':'unset'}} >
+							<Stage height={this.state.height} width={this.state.width} style={{cursor:this.state.isDragging?'grabbing':(this.state.isHovered?'grab':(image.has_label==="false"?'crosshair':'unset'))}}  onClick={e => {
+								// console.log(e);
+								if(image.has_label==='false')
+								{let imageCopy = JSON.parse(JSON.stringify(this.state.image))
+									//make changes to ingredients
+									imageCopy.x= e.evt.offsetX-25,
+									imageCopy.y= e.evt.offsetY-25,
+									imageCopy.height=50,
+									imageCopy.width=50//whatever new ingredients are
+									this.setState({
+										isDragging: false,
+										image: imageCopy
+									});}
+							}}>
 								<Layer>
 									<Rect
 										name="label"
-										x={image.x||this.state.width/2}
-										y={image.y||this.state.height/2}
-										width={image.width||50}
-										height={image.height||50}
+										x={image.x}
+										y={image.y}
+										width={image.width}
+										height={image.height}
 										draggable
 										onMouseOver={()=>{this.setState({isHovered:true})}}
 										onMouseOut={()=>{this.setState({isHovered:false})}}
 										onDragStart={() => {
 											this.setState({
-												isDragging: true
+												isDragging: true,
+												isHovered: false
 											});
 										}}
 										onDragEnd={e => {
@@ -115,6 +129,7 @@ export default class LabelImage extends Component {
 											imageCopy.y= Math.round(e.target.y()) //whatever new ingredients are
 											this.setState({
 												isDragging: false,
+												isHovered: true,
 												image: imageCopy
 											});
 										}}
